b28af4de7b8833274d7370735c2fa9d3c91e9ce6
wangqianqian1100 <17343030061@163.com>
2019-10-25 14:47:27 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,42 @@
 </template>
 
 <style lang="less">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+*{
+		-webkit-tap-highlight-color: rgba(0,0,0,0);
+	}//解决苹果端点击出现黑框
+	body{
+		font-family:"黑体";
+	}
+	html,body{
+		width: 100%;
+		height: 100%;
+	}
+	body,h1,h3,h2,h4,h5,h6,p,dl,dd,ul,ol,form,input,fieldset,th,td,iframe{
+		margin:0; 
+		padding:0;
+	}
+	h1,h2,h3,h4,h5,h6,strong,b{
+		font-weight:normal;
+	}
+	i,em{
+		font-style:normal;
+	}
+	a{
+		text-decoration:none;
+		color:#666;
+	}
+	li{
+		list-style:none;
+	}
+	img{
+		border:none;
+		vertical-align:top;
+		pointer-events:none;
+	}//禁止图片放大
+	input{
+		vertical-align:top;
+	}
+	a,input,area{
+		outline:none;
+	}
 </style>
